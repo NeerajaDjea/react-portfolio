a0060dbbf1fc84614cbe6efa3a6e62786e057434
Neeraja Djearaman <neeru.jai@gmail.com>
2020-02-29 22:39:20 +0000

    project cards

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,33 +1,45 @@
-import React from 'react'
+import React from 'react';
 
-import {Form, Button} from "react-bootstrap";
+import { Form, Button } from 'react-bootstrap';
 function Contact() {
-    return (
-
-<div class="main-container">
-      <form action="mailto:neeru.jai@gmail.com" method="post" enctype="text/plain">
-        <div class="main-container">
-      <h3 class="sub-heading">Contact Me</h3>
-      <div class="group">
-              <label for="input-name" class="label"> Name  </label>
-              <input id="input" name="name" class="input"/>
+  return (
+    <div class='main-container'>
+      <form
+        action='mailto:neeru.jai@gmail.com'
+        method='post'
+        enctype='text/plain'
+      >
+        <div class='main-container'>
+          <h3 class='sub-heading'>Contact Me</h3>
+          <div class='group'>
+            <label for='input-name' class='label'>
+              {' '}
+              Name{' '}
+            </label>
+            <input id='input' name='name' class='input' />
           </div>
-          <div class="group">
-              <label for="input-email" class="label"> Email  </label>
-              <input id="input" name="email" class="input"/>
+          <div class='group'>
+            <label for='input-email' class='label'>
+              {' '}
+              Email{' '}
+            </label>
+            <input id='input' name='email' class='input' />
           </div>
-          <div class="group">
-              <label for="input-message" class="label">Message</label>
-              <input id="input" name="message" class="input"/>
+          <div class='group'>
+            <label for='input-message' class='label'>
+              Message
+            </label>
+            <input id='input' name='message' class='input' />
           </div>
           <div>
-              <button class="button"><strong>Submit</strong></button>
-
+            <button class='button'>
+              <strong>Submit</strong>
+            </button>
           </div>
-          </div>
+        </div>
       </form>
-      </div>
+    </div>
+  );
+}
 
-    )}
-
-export default Contact
+export default Contact;
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,34 +1,38 @@
 import React, { Component } from 'react';
-import Fitness from "../img/fitness.png";
-import Employee from"../img/employee.png";
-import Burger from "../img/burger.png";
-import notes from "../img/notetaker.png";
-import restaurant from "../img/restaurant.png";
-import ProjectCard from "./ProjectCard";
-import ProjectData from "../ProjectData.json"
+import Fitness from '../img/fitness.png';
+import Employee from '../img/employee.png';
+import Burger from '../img/burger.png';
+import notes from '../img/notetaker.png';
+import restaurant from '../img/restaurant.png';
+import ProjectCard from './ProjectCard';
+import ProjectData from '../ProjectData.json';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { CardDeck } from 'react-bootstrap';
+import '../App.css';
 
 export class Projects extends Component {
-    state = {
-        ProjectData 
-      };
-    
-    render() {
-        
-        return (
-            <div>
-            <h1> Projects </h1>
-            {this.state.ProjectData.map(Project => ( 
-        <ProjectCard 
-        title = {Project.title}
-        codeUrl = {Project.codeUrl}
-        appUrl = {Project.appUrl}
-        img = {Project.img}
-        details = {Project.details}
-        />
-          
-             ) )}
-             </div>
-        );
-}}
+  state = {
+    ProjectData
+  };
 
-export default Projects
+  render() {
+    return (
+      <div>
+        <h1> Projects </h1>
+        <CardDeck>
+          {this.state.ProjectData.map(Project => (
+            <ProjectCard
+              title={Project.title}
+              codeUrl={Project.codeUrl}
+              appUrl={Project.appUrl}
+              img={Project.img}
+              details={Project.details}
+            />
+          ))}
+        </CardDeck>
+      </div>
+    );
+  }
+}
+
+export default Projects;
