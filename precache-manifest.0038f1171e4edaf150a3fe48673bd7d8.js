6c084d1e4555cdabc7088efe1fa405abc37ca780
Neeraja Djearaman <neeru.jai@gmail.com>
2020-04-11 09:11:39 +0100

    Updates

---
+++ precache-manifest.0038f1171e4edaf150a3fe48673bd7d8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a8baea28275045ad679c3121c8020b1",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "ba9fdfd1c242e6cfc12f",
+    "url": "/react-portfolio/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c9ac04dc5e526130ab28",
+    "url": "/react-portfolio/static/css/main.c39c508a.chunk.css"
+  },
+  {
+    "revision": "ba9fdfd1c242e6cfc12f",
+    "url": "/react-portfolio/static/js/2.e22db6f7.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/react-portfolio/static/js/2.e22db6f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9ac04dc5e526130ab28",
+    "url": "/react-portfolio/static/js/main.6ddf40e3.chunk.js"
+  },
+  {
+    "revision": "8fcb5e95394b0c7c8de9",
+    "url": "/react-portfolio/static/js/runtime-main.025852de.js"
+  },
+  {
+    "revision": "2a1a9912481105b1d38ca9756e3f3075",
+    "url": "/react-portfolio/static/media/abstract.2a1a9912.png"
+  },
+  {
+    "revision": "8217bd96c4f6029cabd825ac4fe98652",
+    "url": "/react-portfolio/static/media/cv.8217bd96.png"
+  },
+  {
+    "revision": "70e45a5af44837a10e85cf3753acfd15",
+    "url": "/react-portfolio/static/media/pic.70e45a5a.png"
+  }
+]);
